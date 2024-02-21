2d97ae4325ef0a22ee443f5ac735c56d98c4e318
Keana Cobarde <keanacobarde@gmail.com>
2024-02-20 19:43:56 -0600

    Attempting class structure

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,6 +1,17 @@
-﻿namespace Bangazon.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public string PaymentId { get; set; }
+        [Required]
+        public bool IsOrderOpen { get; set; }
+        [Required]
+        public ICollection<Product> Products { get; set; }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,19 @@
-﻿namespace Bangazon.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int QuantityAvailable { get; set; }
+        public float Price { get; set; }
+        public int SellerId { get; set; }
+        public int CategoryId { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
     }
 }
