55d46ab7a38e10e3409193f1f1eb146c4a9bef48
Keana Cobarde <keanacobarde@gmail.com>
2024-02-21 22:57:02 -0600

    Segmenting and creating baseline product API calls

---
+++ Controllers/Products.cs
@@ -0,0 +1,27 @@
+﻿using Bangazon.Models;
+
+namespace Bangazon.Controllers
+{
+    public class Products
+    {
+        public static void Map(WebApplication app)
+        {
+            // GETTING ALL PRODUCTS
+            app.MapGet("/products", (BangazonDbContext db) =>
+            {
+                return db.Products.ToList();
+            });
+            
+            // GETTING ALL PRODUCTS GIVEN AN ID
+            app.MapGet("/products/{id}", (BangazonDbContext db, int id) =>
+            {
+                Product selectedProduct = db.Products.FirstOrDefault(p => p.Id == id);
+                if (selectedProduct == null)
+                { 
+                    return Results.NotFound();
+                }
+                return Results.Ok(selectedProduct);
+            });
+        }
+    };
+}
