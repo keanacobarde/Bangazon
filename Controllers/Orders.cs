59ae215c437a5c18df6012001247d8cf21be31b7
Keana Cobarde <keanacobarde@gmail.com>
2024-02-23 22:37:25 -0600

    Adding needed order API calls from past merge conflicts

---
+++ Controllers/Orders.cs
@@ -0,0 +1,29 @@
+﻿using Bangazon.Models;
+
+namespace Bangazon.Controllers
+{
+    public class ORDERS
+    {
+        public static void Map(WebApplication app)
+        {
+            // GETTING ALL ORDERS
+            app.MapGet("/orders", (BangazonDbContext db) => 
+            { 
+                return db.Orders.ToList();
+            });
+
+            // GETTING ALL ORDERS GIVEN AN ID, DELETING ORDERS
+            app.MapGet("/orders/{id}", (BangazonDbContext db, int id) =>
+            {
+                Order selectedOrder = db.Orders.FirstOrDefault(p => p.Id == id);
+                if (selectedOrder == null)
+                {
+                    return Results.NotFound();
+                }
+                db.Orders.Remove(selectedOrder);
+                db.SaveChanges();
+                return Results.Ok(db.Orders);
+            });
+        }
+    }
+}
